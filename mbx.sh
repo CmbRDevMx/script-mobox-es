b37620885557eb77fa99f9f5409f512d4dc122a5
CmbR <164084699+CmbRDevMx@users.noreply.github.com>
2024-09-04 21:17:12 -0700

    Update mbx.sh

--- mbx.sh
+++ mbx.sh
@@ -9,6 +9,7 @@ NC='\033[0m'
 clear
 echo -e "${V}Change custom wine${NC}"
 echo ""
+echo "Hi 👀"
 echo -e "${A}Example:${NC} ${V}your-wine.tar.xz${NC}"
 read -p $'\e[96minsert Full WineCustom name: \e[0m' name_wine
 read -p $'\e[96minsert Name folder: \e[0m]' random_name 
